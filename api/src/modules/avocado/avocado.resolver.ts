6a649ff36710e6d8d958167026640e78e592b79c
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-02-06 11:21:08 -0400

    add resolvers

---
+++ api/src/modules/avocado/avocado.resolver.ts
@@ -0,0 +1,69 @@
+export default {
+  listAvos() {
+    return [
+      {
+        url: 'XXXXXXXXXXXXXXXXXX',
+        name: 'XXXXXXXXXXXXXXXXXX',
+        title: 'XXXXXXXXXXXXXXXXXX',
+        price: 123,
+        image: 'XXXXXXXXXXXXXXXXXX',
+        SKU: 'XXXXXXXXXXXXXXXXXX',
+        description: 'XXXXXXXXXXXXXXXXXX',
+        attributes: {
+          Shape: 'XXXXXXXXXXXXXXXXXX',
+          Hardiness: 123,
+          Taste: 'XXXXXXXXXXXXXXXXXX',
+        }
+      },
+      {
+        url: 'XXXXXXXXXXXXXXXXXX',
+        name: 'XXXXXXXXXXXXXXXXXX',
+        title: 'XXXXXXXXXXXXXXXXXX',
+        price: 123,
+        image: 'XXXXXXXXXXXXXXXXXX',
+        SKU: 'XXXXXXXXXXXXXXXXXX',
+        description: 'XXXXXXXXXXXXXXXXXX',
+        attributes: {
+          Shape: 'XXXXXXXXXXXXXXXXXX',
+          Hardiness: 123,
+          Taste: 'XXXXXXXXXXXXXXXXXX',
+        }
+      },
+      {
+        url: 'XXXXXXXXXXXXXXXXXX',
+        name: 'XXXXXXXXXXXXXXXXXX',
+        title: 'XXXXXXXXXXXXXXXXXX',
+        price: 123,
+        image: 'XXXXXXXXXXXXXXXXXX',
+        SKU: 'XXXXXXXXXXXXXXXXXX',
+        description: 'XXXXXXXXXXXXXXXXXX',
+        attributes: {
+          Shape: 'XXXXXXXXXXXXXXXXXX',
+          Hardiness: 123,
+          Taste: 'XXXXXXXXXXXXXXXXXX',
+        }
+      },
+    ]
+  },
+
+  oneAvo() {
+    return {
+      url: 'XXXXXXXXXXXXXXXXXX',
+      name: 'XXXXXXXXXXXXXXXXXX',
+      title: 'XXXXXXXXXXXXXXXXXX',
+      price: 123,
+      image: 'XXXXXXXXXXXXXXXXXX',
+      SKU: 'XXXXXXXXXXXXXXXXXX',
+      description: 'XXXXXXXXXXXXXXXXXX',
+      attributes: {
+        Shape: 'XXXXXXXXXXXXXXXXXX',
+        Hardiness: 123,
+        Taste: 'XXXXXXXXXXXXXXXXXX',
+      }
+    }
+  },
+
+  addAvo() {
+    console.log('Create avo');
+  }
+}
--- api/src/resolvers.ts
+++ api/src/resolvers.ts
@@ -1,74 +1,11 @@
+import * as avo from './modules/avocado/avocado.resolver'
+
 export default {
   Query: {
-    info: () => 'This is the API of a Hackernews Clone',
-    avos: () => {
-      return [
-        {
-          url: 'XXXXXXXXXXXXXXXXXX',
-          name: 'XXXXXXXXXXXXXXXXXX',
-          title: 'XXXXXXXXXXXXXXXXXX',
-          price: 123,
-          image: 'XXXXXXXXXXXXXXXXXX',
-          SKU: 'XXXXXXXXXXXXXXXXXX',
-          description: 'XXXXXXXXXXXXXXXXXX',
-          attributes: {
-            Shape: 'XXXXXXXXXXXXXXXXXX',
-            Hardiness: 123,
-            Taste: 'XXXXXXXXXXXXXXXXXX',
-          }
-        },
-        {
-          url: 'XXXXXXXXXXXXXXXXXX',
-          name: 'XXXXXXXXXXXXXXXXXX',
-          title: 'XXXXXXXXXXXXXXXXXX',
-          price: 123,
-          image: 'XXXXXXXXXXXXXXXXXX',
-          SKU: 'XXXXXXXXXXXXXXXXXX',
-          description: 'XXXXXXXXXXXXXXXXXX',
-          attributes: {
-            Shape: 'XXXXXXXXXXXXXXXXXX',
-            Hardiness: 123,
-            Taste: 'XXXXXXXXXXXXXXXXXX',
-          }
-        },
-        {
-          url: 'XXXXXXXXXXXXXXXXXX',
-          name: 'XXXXXXXXXXXXXXXXXX',
-          title: 'XXXXXXXXXXXXXXXXXX',
-          price: 123,
-          image: 'XXXXXXXXXXXXXXXXXX',
-          SKU: 'XXXXXXXXXXXXXXXXXX',
-          description: 'XXXXXXXXXXXXXXXXXX',
-          attributes: {
-            Shape: 'XXXXXXXXXXXXXXXXXX',
-            Hardiness: 123,
-            Taste: 'XXXXXXXXXXXXXXXXXX',
-          }
-        },
-      ]
-    },
-    avo: (_: any, { id }: any) => {
-      console.log(id);
-
-      return {
-        url: 'XXXXXXXXXXXXXXXXXX',
-        name: 'XXXXXXXXXXXXXXXXXX',
-        title: 'XXXXXXXXXXXXXXXXXX',
-        price: 123,
-        image: 'XXXXXXXXXXXXXXXXXX',
-        SKU: 'XXXXXXXXXXXXXXXXXX',
-        description: 'XXXXXXXXXXXXXXXXXX',
-        attributes: {
-          Shape: 'XXXXXXXXXXXXXXXXXX',
-          Hardiness: 123,
-          Taste: 'XXXXXXXXXXXXXXXXXX',
-        }
-      }
-    }
+    avos: () => avo.default.listAvos,
+    avo: () => avo.default.oneAvo
   },
   Mutation: {
-    addAvo: (_: any, {name}: any) => {
-      console.log(name);
-    }
+    addAvo: (_: any, {name}: any) => avo.default.addAvo
   }
 }
