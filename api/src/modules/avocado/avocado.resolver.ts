57ac7639dd9a8685e4c5d3b5920c688bffaaf145
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-02-06 13:56:52 -0400

    resolvers to mutations and filters

--- api/src/modules/avocado/avocado.resolver.ts
+++ api/src/modules/avocado/avocado.resolver.ts
@@ -1,38 +1,28 @@
 import { createHash } from 'crypto'
 import { baseModelResolver } from '../base/base.resolver'
-import { Avocado } from './avocado.model'
+import type { Avocado, Attributes, PrismaClient } from "@prisma/client";
 
-const avos: Avocado[] = [
-  {
-    createdAt: new Date(),
-    updatedAt: undefined,
-    deletedAt: undefined,
-    name: 'Pinkerton Avocado',
-    id: 'fpr72m9k',
-    sku: 'B4HZ42TM',
-    price: 1.27,
-    image: '/images/pinkerton.jpg',
-    attributes: {
-      description:
-        "First grown on the Pinkerton Ranch in Saticoy, California, in the early 1970s, 'Pinkerton' is a seedling of 'Hass' x 'Rincon'. The large fruit has a small seed, and its green skin deepens in color as it ripens. The thick flesh has a smooth, creamy texture, pale green color, good flavor, and high oil content. It shows some cold tolerance, to −1 °C (30 °F) and bears consistently heavy crops. A hybrid Guatemalan type, it has excellent peeling characteristics",
-      shape: 'Long pear',
-      hardiness: '−1 °C',
-      taste: 'Marvelous, is an avocado',
-    },
-  },
-]
-
-export function findAll(): Avocado[] {
-  return avos
+type ResolverContext = {
+  orm: PrismaClient
 }
 
-export function findOne(id: string): Avocado | null {
-  return avos[0]
+
+export function findAll(parent: unknown, args: unknown, context: ResolverContext): Promise<Avocado[]> {
+  return context.orm.avocado.findMany()
 }
 
-export const resolver: Record<keyof Avocado, (parent: Avocado) => unknown> = {
-  ...baseModelResolver,
+// export function findOne(id: string): Avocado | null {
+//   return avos[0]
+// }
+
+export const resolver: Record<keyof
+  (Avocado & {attributes: Attributes}),
+  (parent: (Avocado & {attributes: Attributes}))
+  => unknown> = {
   id: (parent) => parent.id,
+  createdAt: (parent) => parent.createdAt,
+  updatedAt: (parent) => parent.updatedAt,
+  deletedAt: (parent) => parent.deletedAt,
   sku: (parent) => parent.sku,
   name: (parent) => parent.name,
   price: (parent) => parent.price,
@@ -45,33 +35,33 @@ export const resolver: Record<keyof Avocado, (parent: Avocado) => unknown> = {
   }),
 }
 
-export function createAvo(
-  parent: unknown,
-  {
-    data,
-  }: { data: Pick<Avocado, 'name' | 'price' | 'image'> & Avocado['attributes'] }
-): Avocado {
-  const currentLength = avos.length
-  const newAvo: Avocado = {
-    id: String(currentLength + 1),
-    sku: createHash('sha256')
-      .update(data.name, 'utf8')
-      .digest('base64')
-      .slice(-6),
-    createdAt: new Date(),
-    updatedAt: new Date(),
-    deletedAt: undefined,
-    name: data.name,
-    price: data.price,
-    image: data.image,
-    attributes: {
-      description: data.description,
-      shape: data.shape,
-      hardiness: data.hardiness,
-      taste: data.taste,
-    },
-  }
+// export function createAvo(
+//   parent: unknown,
+//   {
+//     data,
+//   }: { data: Pick<Avocado, 'name' | 'price' | 'image'> & Avocado['attributes'] }
+// ): Avocado {
+//   const currentLength = avos.length
+//   const newAvo: Avocado = {
+//     id: (currentLength + 1),
+//     sku: createHash('sha256')
+//       .update(data.name, 'utf8')
+//       .digest('base64')
+//       .slice(-6),
+//     createdAt: new Date(),
+//     updatedAt: new Date(),
+//     deletedAt: null,
+//     name: data.name,
+//     price: data.price,
+//     image: data.image,
+//     attributes: {
+//       description: data.description,
+//       shape: data.shape,
+//       hardiness: data.hardiness,
+//       taste: data.taste,
+//     },
+//   }
 
-  avos.push(newAvo)
-  return newAvo
-}
+//   avos.push(newAvo)
+//   return newAvo
+// }
